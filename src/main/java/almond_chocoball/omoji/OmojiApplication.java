8ef5567972fff5dcd372c6efab2f4b1a08ee1709
linho <linho301150@gmail.com>
2022-11-18 15:24:18 +0900

    [INIT] spring boot setting

---
+++ src/main/java/almond_chocoball/omoji/OmojiApplication.java
@@ -0,0 +1,13 @@
+package almond_chocoball.omoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/almond_chocoball/omoji/OmojiApplicationTests.java
@@ -0,0 +1,13 @@
+package almond_chocoball.omoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
