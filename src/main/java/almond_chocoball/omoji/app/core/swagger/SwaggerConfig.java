c69d2a01d96fd80242bd9238b14630c8ef0e0d9a
linho <linho301150@gmail.com>
2022-11-26 15:52:43 +0900

    [INIT] Setting Swagger3 with redirect

---
+++ src/main/java/almond_chocoball/omoji/app/core/swagger/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package almond_chocoball.omoji.app.core.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .groupName("Almond choco ball")
+                .apiInfo(this.apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Omoji API document")
+                .description("We are Almond choco ball")
+                .version("0.1")
+                .build();
+    }
+}
