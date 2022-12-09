bea9c99f9502273d1ab26f19d290eac5f130157c
young <jinyoung7165@gmail.com>
2022-12-09 17:02:58 +0900

    [INIT] ApiResponse, ErrorAdvice

---
+++ src/main/java/almond_chocoball/omoji/app/common/dto/ErrorResponse.java
@@ -0,0 +1,10 @@
+package almond_chocoball.omoji.app.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String msg; //err내용
+}
