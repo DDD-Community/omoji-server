bc42093a516a1d584fb5c59fec6238c78b8962a4
linho <linho301150@gmail.com>
2022-12-10 01:42:28 +0900

    [FIX] 엔티티 추가에 따른 OneToMany 결합

--- src/main/java/almond_chocoball/omoji/app/domain/entity/PostEntity.java
+++ src/main/java/almond_chocoball/omoji/app/domain/entity/PostEntity.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -23,9 +25,16 @@ public class PostEntity {
     @Column(nullable = false)
     private Long number_dislike = 0L;
 
+    @OneToMany(mappedBy = "post")
+    private List<ImageEntity> images = new ArrayList<>();
+
     @Builder
     public PostEntity(Long number_like,Long number_dislike) {
         this.number_like = number_like;
         this.number_dislike = number_dislike;
+    }
+
+    public void addImage(ImageEntity image) {
+        this.images.add(image);
     }
 }
