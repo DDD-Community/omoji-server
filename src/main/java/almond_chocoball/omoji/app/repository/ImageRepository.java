e3ead6901daa6aadf3579ae6e8f84ad20c6f3d41
linho <linho301150@gmail.com>
2022-12-10 13:13:28 +0900

    [FIX] 중복되는 코드 제거

--- src/main/java/almond_chocoball/omoji/app/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package almond_chocoball.omoji.app.repository;
-
-import almond_chocoball.omoji.app.domain.entity.PostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<PostEntity,Long> {
-}
