65265d95d0197aed9f557212203429071e292f7b
linho <linho301150@gmail.com>
2022-12-30 02:45:22 +0900

    [COMMENT] Swagger Tag명 변경

--- src/main/java/almond_chocoball/omoji/app/evaluate/controller/EvaluateController.java
+++ src/main/java/almond_chocoball/omoji/app/evaluate/controller/EvaluateController.java
@@ -13,13 +13,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
-@Tag(name = "Post reaction",description = "Post를 평가하는 페이지")
+@Tag(name = "Evaluate reaction",description = "Post를 평가하는 페이지")
 public class EvaluateController {
 
     private final EvaluateService evaluateService;
 
     @PostMapping("/reactions")
-    @Tag(name = "Post reaction")
+    @Tag(name = "Evaluate reaction")
     @Operation(summary = "평가하기", description = "Post ID를 입력받고 좋아요(LIKE), 싫어요(DISLIKE), 넘어가기(PASS)를 선택항 값 증가")
     public ResponseEntity<EvaluateResponseDto> increaseLike(EvaluateRequestDto evaluateRequestDto) {
         return ApiResponse.success(evaluateService.setEvaluate(
@@ -29,7 +29,7 @@ public class EvaluateController {
     }
 
     @DeleteMapping("/reactions")
-    @Tag(name = "Post reaction")
+    @Tag(name = "Evaluate reaction")
     @Operation(summary = "평가 제거하기", description = "Post ID를 입력받고 좋아요(LIKE), 싫어요(DISLIKE), 넘어가기(PASS)를 선택하여 값 감소")
     public ResponseEntity<EvaluateResponseDto> increaseDislike(EvaluateRequestDto evaluateRequestDto){
         return ApiResponse.success(evaluateService.setEvaluate(
