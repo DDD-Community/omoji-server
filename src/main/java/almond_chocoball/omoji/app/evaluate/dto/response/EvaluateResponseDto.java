242486cafeeaff3518c9c11d1aa5b44b2c869a7e
linho <linho301150@gmail.com>
2023-01-14 23:05:56 +0900

    [FIX] 사용자에게 반환하는 DTO 변경 (id만 단순하게)

--- src/main/java/almond_chocoball/omoji/app/evaluate/dto/response/EvaluateResponseDto.java
+++ src/main/java/almond_chocoball/omoji/app/evaluate/dto/response/EvaluateResponseDto.java
@@ -1,8 +1,6 @@
 package almond_chocoball.omoji.app.evaluate.dto.response;
 
 import almond_chocoball.omoji.app.evaluate.enums.EvaluateEnum;
-import almond_chocoball.omoji.app.member.entity.Member;
-import almond_chocoball.omoji.app.post.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -15,10 +13,10 @@ public class EvaluateResponseDto {
     private Long id;
 
     @NotNull
-    private Member member;
+    private Long memberId;
 
     @NotNull
-    private Post post;
+    private Long postId;
 
     @NotNull
     private EvaluateEnum evaluateEnum;
