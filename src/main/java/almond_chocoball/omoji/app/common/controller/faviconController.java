d313145eff6e91d649f742164078fc8f7271ab30
young <jinyoung7165@gmail.com>
2023-01-30 04:06:59 +0900

    [FIX] favicon 404

---
+++ src/main/java/almond_chocoball/omoji/app/common/controller/faviconController.java
@@ -0,0 +1,14 @@
+package almond_chocoball.omoji.app.common.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+public class faviconController {
+
+    @GetMapping("favicon.ico")
+    @ApiIgnore
+    public void returnNoFavicon() {
+    }
+}
