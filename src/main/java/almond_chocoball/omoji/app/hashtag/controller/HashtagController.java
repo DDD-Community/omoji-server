1e2fc5f9c9f4725fe759fc338bcbdbd3f50f7cbe
young <jinyoung7165@gmail.com>
2023-01-29 16:35:13 +0900

    [FIX] rn과 security연결 시도

---
+++ src/main/java/almond_chocoball/omoji/app/hashtag/controller/HashtagController.java
@@ -0,0 +1,17 @@
+package almond_chocoball.omoji.app.hashtag.controller;
+
+//@RestController
+//@RequestMapping("/api/v1/hashtags")
+//@RequiredArgsConstructor
+//@Tag(name = "Hashtag", description = "Hashtag를 조회하고 생성합니다")
+//public class HashtagController {
+//
+//    private final HashtagService hashtagService;
+//
+//    @Tag(name = "Hashtag")
+//    @GetMapping()
+//    @Operation(summary = "전체 Hashtag 리스트 조회", description = "Hashtag를 조회하는 API")
+//    public ResponseEntity<DetailPostResponseDto> getHashtags() {
+//        return ApiResponse.success(postService.getPost(id));
+//    }
+//}
