24028e038fe47c56bb03ae89c6b555c95b26f3d0
young <jinyoung7165@gmail.com>
2023-01-30 17:57:26 +0900

    [INIT] db create

--- src/main/java/almond_chocoball/omoji/app/common/db/initHashtagDb.java
+++ src/main/java/almond_chocoball/omoji/app/common/db/initHashtagDb.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
 
-@Profile("local")
+@Profile("prod")
 @Component
 @RequiredArgsConstructor
 public class initHashtagDb {
